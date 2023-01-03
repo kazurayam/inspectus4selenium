b80c98f0ad242dcb4ac2635fefc06a9e0a7ecd03
kazurayam <kazuaki.urayama@gmail.com>
2023-01-03 10:20:26 +0900

    #25, #26

--- src/main/java/com/kazurayam/inspectus/materialize/url/URLMaterializingFunction.java
+++
@@ -1,8 +0,0 @@
-package com.kazurayam.inspectus.materialize.url;
-
-import com.kazurayam.materialstore.core.filesystem.MaterialstoreException;
-
-public interface URLMaterializingFunction<Target, Material> {
-
-    Material accept(Target target) throws MaterialstoreException;
-}
